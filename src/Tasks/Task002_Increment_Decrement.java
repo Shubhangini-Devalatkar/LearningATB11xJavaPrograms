dcf9e0e5b1037777e9f38b3529544c972fa4e713
Shubhangini <shubhangini.devalatkar@gmail.com>
2025-04-18 22:16:37 +0530

    14 March 2025 - New Files added

--- src/Tasks/Task002_Increment_Decrement.java
+++ src/Tasks/Task002_Increment_Decrement.java
@@ -3,7 +3,11 @@ package Tasks;
 public class Task002_Increment_Decrement {
     public static void main(String[] args) {
         int a = 10;
+
+
         System.out.println(++a + a++ + a++);
+
+
         System.out.println(a);
 
         int a1 = 20;
---
+++ src/ex_08_If_Condition/Lab080_If.java
@@ -0,0 +1,11 @@
+package ex_08_If_Condition;
+
+public class Lab080_If {
+    public static void main(String[] args) {
+        int age = 16;
+        if(age > 18) {
+            System.out.println(" you can vote");
+
+        }
+    }
+}
